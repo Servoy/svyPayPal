a88482328cc3851b165332547611441e71aa9f05
paronne <paronne@servoy.com>
2014-07-04 11:05:26 +0200

    change report_coverage path

--- svyPayPal_test/svyPayPal_test.js
+++ svyPayPal_test/svyPayPal_test.js
@@ -113,7 +113,7 @@ function onSolutionClose(force) {
 	
 	if (coverageExists) {
 		// TODO change file path
-		var filePath = "C:\\Program Files (x86)\\Jenkins\\jobs\\svyPayPal\\workspace\\PayPal\\svyPayPal_test\\CBI_config\\report_coverage\\coverage.json"
+		var filePath = "C:\\Program Files (x86)\\Jenkins\\jobs\\svyPayPal\\workspace\\JenkinsConfig\\svyJenkinsConfig\\CBI_config\\report_coverage\\coverage.json"
 		var jsFile = plugins.file.createFile(filePath)
 		if (!plugins.file.writeTXTFile(jsFile,JSON.stringify(__coverage__),'UTF-8','json')) {
 			log.error('Cannot write file ' + filePath)
